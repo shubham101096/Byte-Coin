cb98d5fcbcef8739c4b782b0d1f0776a421ea00f
Shubham Mishra <shubhammishra10@gmail.com>
2021-06-29 19:18:08 +0530

    Code Cleaning

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -18,7 +18,7 @@ protocol CoinManagerDelegate {
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "9649C778-E8AD-40E5-B6A2-4194BBA8392A"
+    let apiKey = "API_KEY"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
